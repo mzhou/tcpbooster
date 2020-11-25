957d14e72eb549068b397c550e675dcec36341de
Michael Zhou <mzhou@cse.unsw.edu.au>
2020-11-25 21:24:18 +1100

    add socks support

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use std::error::Error;
 use std::os::unix::io::{AsRawFd, FromRawFd};
 
 use clap::{App, Arg};
@@ -57,10 +58,48 @@ fn create_bound_socket(addr: &std::net::SocketAddr) -> nix::Result<std::net::Tcp
     return Ok(sock);
 }
 
+#[derive(Clone)]
+struct SocksConfig {
+    server: SocketAddr,
+    username: String,
+    password: String,
+}
+
+#[derive(Clone)]
+struct Config {
+    bind: Option<SocketAddr>,
+    socks: Option<SocksConfig>,
+}
+
+// TODO: proper error bubbling
+async fn create_outgoing_socket(
+    socks: Option<SocksConfig>,
+    bind_addr: &std::net::SocketAddr,
+    connect_addr: &std::net::SocketAddr,
+) -> Result<TcpStream, Box<dyn Error>> {
+    let std_out_sock = create_bound_socket(bind_addr)?;
+    if let Some(socks) = socks {
+        let tokio_out_sock = TcpStream::connect_std(std_out_sock, &socks.server).await?;
+        tokio_socks::tcp::Socks5Stream::connect_with_password_and_socket(
+            tokio_out_sock,
+            connect_addr,
+            socks.username.as_str(),
+            socks.password.as_str(),
+        )
+        .await
+        .map(|s| s.into_inner())
+        .map_err(|e| e.into())
+    } else {
+        TcpStream::connect_std(std_out_sock, &connect_addr)
+            .await
+            .map_err(|e| e.into())
+    }
+}
+
 async fn accept_loop_inner(
     mut listener: TcpListener,
     banned_port: u16,
-    bind: Option<SocketAddr>,
+    config: Config,
 ) -> Result<(), tokio::io::Error> {
     loop {
         let (in_sock, in_remote_addr) = listener.accept().await?;
@@ -71,67 +110,67 @@ async fn accept_loop_inner(
         }
         let in_to_out_log_tag = format!("{}->{} ", in_remote_addr, out_remote_addr);
         println!("{}accept", in_to_out_log_tag);
-        tokio::spawn(async move {
-            if let Err(e) = in_sock.set_nodelay(true) {
-                println!("{}in set_nodelay failed {}", in_to_out_log_tag, e);
-            }
-            match create_bound_socket(&bind.unwrap_or(in_remote_addr)) {
-                Ok(std_out_sock) => {
-                    match TcpStream::connect_std(std_out_sock, &out_remote_addr).await {
-                        Ok(out_sock) => {
-                            println!("{}connect success", in_to_out_log_tag);
-                            if let Err(e) = out_sock.set_nodelay(true) {
-                                println!("{}out set_nodelay failed {}", in_to_out_log_tag, e);
-                            }
-                            let (mut in_read, mut in_write) = in_sock.into_split();
-                            let (mut out_read, mut out_write) = out_sock.into_split();
-
-                            tokio::spawn(async move {
-                                let mut buf = A4k::default();
-                                println!("{}buf {:p}", &in_to_out_log_tag, &buf);
-                                forward_data(
-                                    &in_to_out_log_tag,
-                                    &mut buf,
-                                    &mut in_read,
-                                    &mut out_write,
-                                )
-                                .await;
-                            });
-
-                            tokio::spawn(async move {
-                                let mut buf = A4k::default();
-                                let out_to_in_log_tag =
-                                    format!("{}<-{} ", in_remote_addr, out_remote_addr);
-                                println!("{}buf {:p}", &out_to_in_log_tag, &buf);
-                                forward_data(
-                                    &out_to_in_log_tag,
-                                    &mut buf,
-                                    &mut out_read,
-                                    &mut in_write,
-                                )
-                                .await;
-                            });
-                        }
-                        Err(e) => {
-                            println!("{}connect failed {}", in_to_out_log_tag, e);
-                        }
-                    }
+        tokio::spawn({
+            let config = config.clone();
+            async move {
+                if let Err(e) = in_sock.set_nodelay(true) {
+                    println!("{}in set_nodelay failed {}", in_to_out_log_tag, e);
                 }
-                Err(e) => {
-                    println!("{}bind failed {}", in_to_out_log_tag, e);
+                let bind_addr = config.bind.unwrap_or(in_remote_addr);
+                match create_outgoing_socket(config.socks, &bind_addr, &out_remote_addr).await {
+                    Ok(out_sock) => {
+                        println!("{}connect success", in_to_out_log_tag);
+                        if let Err(e) = out_sock.set_nodelay(true) {
+                            println!("{}out set_nodelay failed {}", in_to_out_log_tag, e);
+                        }
+                        let (mut in_read, mut in_write) = in_sock.into_split();
+                        let (mut out_read, mut out_write) = out_sock.into_split();
+
+                        tokio::spawn(async move {
+                            let mut buf = A4k::default();
+                            println!("{}buf {:p}", &in_to_out_log_tag, &buf);
+                            forward_data(
+                                &in_to_out_log_tag,
+                                &mut buf,
+                                &mut in_read,
+                                &mut out_write,
+                            )
+                            .await;
+                        });
+
+                        tokio::spawn(async move {
+                            let mut buf = A4k::default();
+                            let out_to_in_log_tag =
+                                format!("{}<-{} ", in_remote_addr, out_remote_addr);
+                            println!("{}buf {:p}", &out_to_in_log_tag, &buf);
+                            forward_data(
+                                &out_to_in_log_tag,
+                                &mut buf,
+                                &mut out_read,
+                                &mut in_write,
+                            )
+                            .await;
+                        });
+                    }
+                    Err(e) => {
+                        println!("{}connect failed {}", in_to_out_log_tag, e);
+                    }
                 }
             }
         });
     }
 }
 
-async fn accept_loop(listener: TcpListener, banned_port: u16, bind: Option<SocketAddr>) {
-    let _ = accept_loop_inner(listener, banned_port, bind).await;
+async fn accept_loop(listener: TcpListener, banned_port: u16, config: Config) {
+    let _ = accept_loop_inner(listener, banned_port, config).await;
 }
 
 const BIND: &str = "BIND";
 const PORT1: &str = "PORT1";
 const PORT2: &str = "PORT2";
+const SOCKS_SERVER: &str = "SOCKS_SERVER";
+const SOCKS_USERNAME: &str = "SOCKS_USERNAME";
+const SOCKS_PASSWORD: &str = "SOCKS_PASSWORD";
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -159,19 +198,63 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
                 .short("1")
                 .takes_value(true),
         )
+        .arg(
+            Arg::with_name(SOCKS_PASSWORD)
+                .help("SOCKS password")
+                .long("socks-password")
+                .takes_value(true),
+        )
+        .arg(
+            Arg::with_name(SOCKS_SERVER)
+                .help("SOCKS server")
+                .long("socks-server")
+                .takes_value(true),
+        )
+        .arg(
+            Arg::with_name(SOCKS_USERNAME)
+                .help("SOCKS username")
+                .long("socks-username")
+                .takes_value(true),
+        )
         .get_matches();
-    let bind: Option<SocketAddr> = if let Some(v) = matches.value_of(BIND) { Some(v.parse()?) } else { None };
+    let bind: Option<SocketAddr> = if let Some(v) = matches.value_of(BIND) {
+        Some(v.parse()?)
+    } else {
+        None
+    };
     println!("bind {:?} {:?}", matches.value_of(BIND), bind);
     let port1: u16 = matches.value_of(PORT1).unwrap().parse().unwrap();
     let port2: u16 = matches.value_of(PORT2).unwrap().parse().unwrap();
+    let socks_password = matches.value_of(SOCKS_PASSWORD);
+    let socks_server: Option<SocketAddr> = matches
+        .value_of(SOCKS_SERVER)
+        .map(|v| v.parse())
+        .transpose()?;
+    let socks_username = matches.value_of(SOCKS_USERNAME);
+
+    let socks_config = if let (Some(socks_password), Some(socks_server), Some(socks_username)) =
+        (socks_password, socks_server, socks_username)
+    {
+        Some(SocksConfig {
+            password: String::from(socks_password),
+            server: socks_server,
+            username: String::from(socks_username),
+        })
+    } else {
+        None
+    };
+    let config = Config {
+        bind,
+        socks: socks_config,
+    };
 
     let listener1 = TcpListener::bind(format!("0.0.0.0:{}", port1)).await?;
     setsockopt(listener1.as_raw_fd(), IpTransparent, &true)?;
-    let task1 = tokio::spawn(accept_loop(listener1, port1, bind));
+    let task1 = tokio::spawn(accept_loop(listener1, port1, config.clone()));
 
     let listener2 = TcpListener::bind(format!("0.0.0.0:{}", port2)).await?;
     setsockopt(listener2.as_raw_fd(), IpTransparent, &true)?;
-    let task2 = tokio::spawn(accept_loop(listener2, port2, bind));
+    let task2 = tokio::spawn(accept_loop(listener2, port2, config.clone()));
 
     let _ = task1.await;
     let _ = task2.await;
